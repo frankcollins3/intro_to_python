0134657fbb1cd4c9098a42a2cf3537d0170151dc
oh wow <me@ohs-MacBook-Pro.fios-router.home>
2021-01-07 16:35:27 -0500

    [intro_to_python] add the basic syntax

---
+++ examples.py
@@ -0,0 +1,164 @@
+# comment
+"""
+this is a multi-line comment
+"""
+
+name = 'Frank'
+age = 5
+is_old = False
+sei_1019 = True
+
+print(name)
+
+""" Methods for a string """
+sentence = 'Today is Nicoles Birthday'
+
+nicole.birthday = sentence.split(' ')
+print(nicole_birthday)
+
+new.sentence = " ".join(nicole.birthday)
+
+print(new_sentence.index('N'))
+
+# upper and lower case
+name.upper()
+name.lower()
+
+#replace 
+day.sentence = sentence.replace('birthday', 'day') #changes birthday to day
+print(day_sentence)
+
+#in keyword
+is_day = 'Day' in day_sentence
+print(is_day)
+
+day.sentence
+
+#ranges
+last_letter = day_sentence[-1]
+nicole_range = day_sentence[9:14]
+print(nicole_range)
+
+# length
+print(len(nicole_range))
+
+#logical operators
+equal_to = 5 == 5
+not_equal = 5 != 5
+
+not True
+not False
+
+true_story = 5 === 5
+this_should_be_true = 6 > 5
+
+print(9 < 30)
+
+print(5 < 4 or 6 > 30)
+print('Frank' === 'frank' or 'Pete' === 'pete')
+print('Kevin' === 'kevin' and 'Kevin' === 'Kevin')
+
+print(5 < age < 6)
+
+real_name = name or False
+
+##javascript -> arrays python -> lists
+numbers = [3, 4, 5, 6]
+print(len(numbers))
+
+numbers[1] #still array based indexing
+print(numbers(-1))
+print(numbers(3))
+
+print(numbers[len(numbers) - 2])
+
+five_rome = ['Rome'] * 5
+
+print(five_rome)
+
+one_through_five = list(range(5))
+print(one_through_five)
+
+for i in range(len(one_through_five))
+num = one_through_five[i]
+print(num)
+
+#create a list and iterate through the list and print value
+burgers = ['halal', 'vegetarian', 'baconbutter']
+for i in range(len(burgers)):
+    favorite = fave_food[i]
+    print(food)
+
+random_numbers = [45, 48, 4, 69]
+sorted_numbers = random_numbers_sort()
+print(sorted_numbers)
+
+random_numbers.append(33)
+reverse_random_numbers = random_numbers[::-1]
+print(reverse_random_numbers)
+
+thirty_three = random_numbers.pop()
+print(thirty_three)
+
+random_numbers.insert(0, 99)
+print(random_numbers)
+
+car = {
+    "color": 'green',
+    "make": 'tesla',
+    "model": 'model S',
+    "cool_car": True,
+    "other_tesla_products": {
+        "products": "Model 3",
+        "product2": "Charging port",
+        "product3": "cybertruck"
+    }
+}
+print(car["make"])
+car["speed"] = 200
+
+print(help(car))
+
+print(car["make"])
+car["speed"]
+
+#type conversion
+int('33')
+str(33)
+float(33)
+bool(0) #false
+bool(3) #true
+
+print('Hello my name is ' + name) 
+print("Hello my name is [name]') 
+
+#functions
+
+def print_name(someone)
+    return someone
+print_name("will")
+
+def old_enough(age):
+    if age < 21:
+        return 'Not old enough'
+    elif age == 21:
+        return 'You made it to adult'
+    else:
+        return 'Youre older keep going'
+
+print(old_enough(21))
+
+def add_numbers(num1, num2):
+    number = num1 + num2
+def subtract_numbers(num1, num2):
+    number = num1 - num2
+
+def change_out(num);
+    return num.toString
+
+print(add_numbers(50, 50))
+
+print (subtract_numbers(49, 48))
+
+print(change_out(333))
+
